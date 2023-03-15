85130e3a43d8fde2f1bf252db0944ef21967df85
AlanRandon <alan.randon@outlook.com>
2023-03-15 15:21:46 +0000

    add mason for lsps

--- nvim/lua/cfg/packer.lua
+++ nvim/lua/cfg/packer.lua
@@ -31,6 +31,29 @@ return require("packer").startup(function(use)
         use("ThePrimeagen/vim-be-good")
         use("alec-gibson/nvim-tetris")
         use("nvim-telescope/telescope.nvim")
+        use {
+            "VonHeikemen/lsp-zero.nvim",
+            branch = "v1.x",
+            requires = {
+                -- LSP Support
+                { "neovim/nvim-lspconfig" },
+                { "williamboman/mason.nvim" },
+                { "williamboman/mason-lspconfig.nvim" },
+                -- Autocompletion
+                { "hrsh7th/nvim-cmp" },
+                { "hrsh7th/cmp-nvim-lsp" },
+                { "hrsh7th/cmp-buffer" },
+                { "hrsh7th/cmp-path" },
+                { "saadparwaiz1/cmp_luasnip" },
+                { "hrsh7th/cmp-nvim-lua" },
+                -- Snippets
+                { "L3MON4D3/LuaSnip" },
+                { "rafamadriz/friendly-snippets" },
+            },
+            config = function()
+                require("cfg.lsp")
+            end
+        }
     end
 
 
