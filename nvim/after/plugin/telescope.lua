05e60b3042944c403e3cd0e4e40b40922762d1be
AlanRandon <alan.randon@outlook.com>
2023-04-12 18:52:11 +0100

    add telescope diagnosics keymap

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,4 +6,5 @@ if not vim.g.vscode then
 	vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
 	vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
 	vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
+	vim.keymap.set("n", "<leader>fd", telescope.diagnostics, {})
 end
