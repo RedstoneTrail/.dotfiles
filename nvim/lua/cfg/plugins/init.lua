4d9a3fe6e83d273eee32daea1d0cdf75f2044d69
RedstoneTrail <joebearchell@gmail.com>
2025-01-20 17:42:31 +0000

    moved over some configs from .config

--- nvim/lua/cfg/plugins/init.lua
+++ nvim/lua/cfg/plugins/init.lua
@@ -12,7 +12,7 @@ return {
 		end,
 	},
 	{ "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
-	{ "alec-gibson/nvim-tetris", cmd = "Tetris" },
+	{ "RedstoneTrail/nvim-tetris", cmd = "Tetris" },
 	{ "tpope/vim-fugitive", cmd = "Git", cond = not vim.g.vscode },
 	{ "mbbill/undotree", cmd = "UndotreeToggle" },
 	{ "2kabhishek/co-author.nvim", cmd = "CoAuthor" },
