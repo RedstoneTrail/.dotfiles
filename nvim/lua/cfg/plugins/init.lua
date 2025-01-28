d1027788f15adafa5fa9dde56301782af13752c1
RedstoneTrail <joebearchell@gmail.com>
2025-01-28 15:24:12 +0000

    add ghostty config and small changes

--- nvim/lua/cfg/plugins/init.lua
+++ nvim/lua/cfg/plugins/init.lua
@@ -28,4 +28,19 @@ return {
 		"hat0uma/csvview.nvim",
 		opts = { view = { display_mode = "border" } },
 	},
+	{
+		"kndndrj/nvim-dbee",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		build = function()
+			-- Install tries to automatically detect the install method.
+			-- if it fails, try calling it with one of these parameters:
+			--    "curl", "wget", "bitsadmin", "go"
+			require("dbee").install()
+		end,
+		config = function()
+			require("dbee").setup(--[[optional config]])
+		end,
+	},
 }
