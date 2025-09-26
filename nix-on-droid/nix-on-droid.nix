fc774821602ebbc75aa91361d1acfaf61e067c4b
redstonetrail <joe.bearchell@gmail.com>
2025-09-26 14:48:23 +0100

    add nix-on-droid config

---
+++ nix-on-droid/nix-on-droid.nix
@@ -0,0 +1,67 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.packages = with pkgs; [
+    tmux
+    htop-vim
+    iputils
+    git
+    neovim
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+    glow
+    mpv
+    proxychains
+    openssh
+    github-cli
+    tealdeer
+  ];
+
+  system.stateVersion = "24.05";
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  time.timeZone = "Europe/London";
+
+  #services.tor = {
+  #  enable =true;
+  #  torsocks.enable = true;
+  #  client.enable = true;
+  #  settings = {
+  #     ControlPort = 9051;
+  #  };
+  #};
+
+  android-integration = {
+    termux-setup-storage.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+    xdg-open.enable = true;
+  };
+
+  terminal = {
+    colors = {
+      background = "#212121";
+    };
+    font = "/storage/emulated/0/Download/firamono-nerd-font/FiraMonoNerdFontMono-Regular.otf";
+  };
+}
