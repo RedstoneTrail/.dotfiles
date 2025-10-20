8e2e31444ce9de7e6115f0ae0174b56f7b73f4e5
redstonetrail <joe.bearchell@gmail.com>
2025-10-20 21:29:00 +0100

    add per device nvim config for unipicker; run formatter over nvim config

--- nvim/lua/config/plugins/init.lua
+++ nvim/lua/config/plugins/init.lua
@@ -1,6 +1,31 @@
+local hostname = vim.uv.os_gethostname()
+local unipicker_config = {}
+
+if hostname == "karl" then
+	os.execute('notify-send "' .. hostname .. '"')
+
+	unipicker_config = {
+		"cosmicboots/unicode_picker.nvim",
+		dependencies = {
+			"uga-rosa/utf8.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			local unicode_picker = require("unicode_picker")
+			unicode_picker.setup()
+			vim.keymap.set(
+				"n",
+				"<leader>ui",
+				unicode_picker.unicode_chars,
+				{ buffer = bufnr, desc = "[U]nicode [I]nsert" }
+			)
+		end,
+	}
+end
+
 return {
 	"tpope/vim-commentary",
-	{ "mbbill/undotree",           cmd = "UndotreeToggle" },
+	{ "mbbill/undotree", cmd = "UndotreeToggle" },
 	{
 		"hat0uma/csvview.nvim",
 		opts = { view = { display_mode = "border" } },
@@ -18,7 +43,7 @@ return {
 			columns = {},
 			-- columns = { "icon", "permissions", "size", "mtime", },
 			keymaps = {
-				["<C-p>"] = { "actions.preview", opts = { vertical = true, split = "botright" }, },
+				["<C-p>"] = { "actions.preview", opts = { vertical = true, split = "botright" } },
 			},
 			watch_for_changes = true,
 			view_options = {
@@ -31,33 +56,20 @@ return {
 		},
 	},
 	{
-		'chomosuke/typst-preview.nvim',
+		"chomosuke/typst-preview.nvim",
 		lazy = false,
-		version = '1.*',
+		version = "1.*",
 		opts = {},
-	},
-	{
-		"cosmicboots/unicode_picker.nvim",
-		dependencies = {
-			"uga-rosa/utf8.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			local unicode_picker = require("unicode_picker")
-			unicode_picker.setup()
-			vim.keymap.set("n", "<leader>ui", unicode_picker.unicode_chars,
-				{ buffer = bufnr, desc = "[U]nicode [I]nsert" })
-		end,
 	},
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
 		keys = {
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>",  desc = "[F]ind [F]iles" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>",   desc = "[F]ind [G]rep" },
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>",     desc = "[F]ind [B]uffer" },
-			{ "<leader>fh", "<cmd>Telescope help_tags<cr>",   desc = "[F]ind [H]elp" },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "[F]ind [F]iles" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "[F]ind [G]rep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "[F]ind [B]uffer" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "[F]ind [H]elp" },
 			{ "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "[F]ind [D]iagnostic" },
 		},
 		cmd = "Telescope",
@@ -97,72 +109,72 @@ return {
 		},
 	},
 	{
-	"nvim-lualine/lualine.nvim",
-	dependencies = { { "nvim-tree/nvim-web-devicons", lazy = true } },
+		"nvim-lualine/lualine.nvim",
+		dependencies = { { "nvim-tree/nvim-web-devicons", lazy = true } },
 
-	init = function()
-		local colours = {
-			black = "#000000",
-			red = "#aa0000",
-			green = "#00aa00",
-			yellow = "#aa5500",
-			blue = "#0000aa",
-			magenta = "#aa00aa",
-			cyan = "#00aaaa",
-			white = "#aaaaaa",
-			bblack = "#555555",
-			bred = "#ff5555",
-			bgreen = "#55ff55",
-			byellow = "#ffff55",
-			bblue = "#5555ff",
-			bmagenta = "#ff55ff",
-			bcyan = "#55ffff",
-			bwhite = "#ffffff",
-		}
+		init = function()
+			local colours = {
+				black = "#000000",
+				red = "#aa0000",
+				green = "#00aa00",
+				yellow = "#aa5500",
+				blue = "#0000aa",
+				magenta = "#aa00aa",
+				cyan = "#00aaaa",
+				white = "#aaaaaa",
+				bblack = "#555555",
+				bred = "#ff5555",
+				bgreen = "#55ff55",
+				byellow = "#ffff55",
+				bblue = "#5555ff",
+				bmagenta = "#ff55ff",
+				bcyan = "#55ffff",
+				bwhite = "#ffffff",
+			}
 
-		local custom_theme = {
-			normal = {
-				a = { fg = colours.bwhite, bg = colours.bblack },
-				b = { fg = colours.white, bg = colours.black },
-				c = { fg = colours.white, bg = colours.black },
-				x = { fg = colours.white, bg = colours.black },
-				y = { fg = colours.white, bg = colours.black },
-				z = { fg = colours.bwhite, bg = colours.bblack },
-			},
+			local custom_theme = {
+				normal = {
+					a = { fg = colours.bwhite, bg = colours.bblack },
+					b = { fg = colours.white, bg = colours.black },
+					c = { fg = colours.white, bg = colours.black },
+					x = { fg = colours.white, bg = colours.black },
+					y = { fg = colours.white, bg = colours.black },
+					z = { fg = colours.bwhite, bg = colours.bblack },
+				},
 
-			insert = { a = { fg = colours.bwhite, bg = colours.green } },
-			visual = { a = { fg = colours.bwhite, bg = colours.magenta } },
-			replace = { a = { fg = colours.bwhite, bg = colours.red } },
-			command = { a = { fg = colours.bwhite, bg = colours.cyan } },
-		}
+				insert = { a = { fg = colours.bwhite, bg = colours.green } },
+				visual = { a = { fg = colours.bwhite, bg = colours.magenta } },
+				replace = { a = { fg = colours.bwhite, bg = colours.red } },
+				command = { a = { fg = colours.bwhite, bg = colours.cyan } },
+			}
 
-		require("lualine").setup({
-			options = {
-				icons_enabled = false,
-				theme = custom_theme,
-				component_separators = { left = "/", right = "/" },
-				section_separators = "",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "filename", "branch", "diagnostics" },
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = { "encoding", "filetype", "filesize", "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = { "filename", "branch", "diagnostics" },
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = { "encoding", "filetype", "filesize", "progress" },
-				lualine_z = { "location" },
-			},
-			tabline = {},
-			extensions = { "oil" },
-		})
-	end
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					theme = custom_theme,
+					component_separators = { left = "/", right = "/" },
+					section_separators = "",
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "filename", "branch", "diagnostics" },
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = { "encoding", "filetype", "filesize", "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = { "filename", "branch", "diagnostics" },
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = { "encoding", "filetype", "filesize", "progress" },
+					lualine_z = { "location" },
+				},
+				tabline = {},
+				extensions = { "oil" },
+			})
+		end,
 	},
 	"neovim/nvim-lspconfig",
 	"nvimtools/none-ls.nvim",
@@ -171,4 +183,5 @@ return {
 		ft = "lua",
 		opts = {},
 	},
+	unipicker_config,
 }
