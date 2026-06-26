781832223014943b7b15e3397954bb909ec2fb42
redstonetrail <joe.bearchell@gmail.com>
2026-06-26 22:34:09 +0100

    fix karl multiple user definitions

--- nixos/karl/default.nix
+++ nixos/karl/default.nix
@@ -48,21 +48,5 @@
 
     networking.hostName = "karl";
     system.stateVersion = "25.05";
-
-    users.users.redstonetrail = {
-      isNormalUser = true;
-      extraGroups = [
-        "wheel"
-        "network"
-        "networkmanager"
-        "uinput"
-        "wireshark"
-        # ]
-        # ++ lib.mkIf config.dotfiles.pihole.enabled [
-        #   "docker"
-      ];
-      initialHashedPassword = "$y$j9T$rQ1v1g1IaFIJ6P7UHWsJ91$LjP8EG33DaUg9DTwbU8gbV6/eY4TB/SgUvRMvAphWFD";
-      shell = pkgs.zsh;
-    };
   };
 }
