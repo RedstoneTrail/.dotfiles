3e5ae5a574fce9022ec53a8d140b8b06a981403d
redstonetrail <joe.bearchell@gmail.com>
2026-07-12 20:38:32 +0100

    fix libva for intel on karl

--- nixos/karl/intel.nix
+++ nixos/karl/intel.nix
@@ -9,7 +9,22 @@
     libva-vdpau-driver
     intel-vaapi-driver
     intel-media-driver
+
+    libvdpau-va-gl
   ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      libvdpau-va-gl
+    ];
+  };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
 
   dotfiles.allowed-unfree-packages = [
     "intel-ocl"
